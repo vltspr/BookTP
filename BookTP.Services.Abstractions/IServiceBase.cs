c5e3a16370fb2278c75dbd5c0a508df1f144b063
Corentin PALLAR <corentin.pallard@azeo.com>
2020-10-15 19:42:03 +0200

    [API] Services project, Base service

---
+++ BookTP.Services.Abstractions/IServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookTP.Models;
+
+namespace BookTP.Services.Abstractions
+{
+    interface IServiceBase
+    {
+        Task<List<Shelve>> GetAll();
+        Task<Shelve> GetById(Guid id);
+        Task AddEntity(object entity);
+    }
+}
---
+++ BookTP.Services.Abstractions/IShelveService.cs
@@ -0,0 +1,6 @@
+﻿namespace BookTP.Services
+{
+    interface IShelveService
+    {
+    }
+}
